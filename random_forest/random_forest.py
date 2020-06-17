2a94dea57114ee462dded5278c98a7394308d7cf
Harpal Singh <harpal.singh@webners.com>
2020-06-17 09:17:45 +0530

    Added Decision Tree and Random Forest.

---
+++ random_forest/random_forest.py
@@ -0,0 +1,38 @@
+import numpy as np
+from decision_tree.decision_tree import DecisionTree
+from collections import Counter
+
+def getRandomSet(X, y):
+    n_samples = X.shape[0]
+    idxs = np.random.choice(n_samples, n_samples,replace=True)
+    return X[idxs], y[idxs]
+
+
+def most_common_label(y):
+    counter = Counter(y)
+    most_common = counter.most_common(1)[0][0]
+    return most_common
+
+
+class RandomForest:
+
+    def __init__(self, n_trees = 3, min_samples_split = 2, max_depth = 0, n_feats = None):
+        self.n_trees = n_trees
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_feats = n_feats
+        self.trees = []
+
+    def fit(self, X, y):
+        self.trees = []
+        for _ in range(self.n_trees):
+            X_set, y_set = getRandomSet(X, y)
+            tree = DecisionTree(max_depth = self.max_depth, min_samples_split = self.min_samples_split, n_feats = self.n_feats)
+            tree.fit(X_set, y_set)
+            self.trees.append(tree)
+
+    def predict(self, X):
+        predictions = np.array([tree.predict(X) for tree in self.trees])
+        tree_preds = np.swapaxes(predictions, 0, 1)
+        y_pred = [most_common_label(tree_pred) for tree_pred in tree_preds]
+        return np.array(y_pred)
